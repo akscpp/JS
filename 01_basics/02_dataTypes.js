ee6aae07ab7e5d13bf59c482b7e4cadf7b62378f
Akshit <129672950+akscpp@users.noreply.github.com>
2024-01-06 18:54:25 +0000

    New basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version of JS
+
+// alert("hello") //we are using NodeJS , not browser
+
+console.log(2+2);
+
+console.log(4+4);
+
+//Code Readiblity should be high
+
+// TC39 , MDN
+
+let name = "Akshit"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//number => 2^(53)
+//bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value =>representation of empty value
+// undefined =>
+// symbol => unique
+
+// object 
+console.log(typeof isLoggedIn);//boolean
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
