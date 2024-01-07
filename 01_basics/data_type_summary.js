f8b82846fef200396cf10885cbcb4311a5ffbc1b
Akshit <129672950+akscpp@users.noreply.github.com>
2024-01-07 17:56:31 +0000

    data type summary

---
+++ 01_basics/data_type_summary.js
@@ -0,0 +1,41 @@
+// Primitive -> (Call by value)
+// 7
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// JJavaScript is a dynamically typed language, which means that data types of variables are determined by the value
+//  they hold at runtime and can change throughout the program as we assign different values to them.
+
+const score = 100
+const scoreVal = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;//undefined
+
+// declaring symbols
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id==anotherId); //false
+
+const bigNumber = 77869927726883863739n //(for BigInt)
+
+// Reference Type (Non-Primitive)
+// Array, Objects, Functions                //typedef(NonPrimitive)-> Function
+// Function , Function , Funtion Object
+
+const heroes = ['shaktiman','nagraj','doga']//array
+
+let myObj={                 //object
+    name:"Akshit",
+    age:22,
+}
+
+console.log(myObj.name);
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+console.log(typeof myFunction);
