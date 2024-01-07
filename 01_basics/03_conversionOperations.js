f35be39c8ff456376718b70e1fedab4a05e80868
Akshit <129672950+akscpp@users.noreply.github.com>
2024-01-07 11:46:30 +0000

    conversion of data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueInNum = Number(score)
+
+// console.log(valueInNum);
+
+
+//"33" =>33
+// "33abc" => NaN
+// true => 1 ; false =>0
+
+// let isLoggedIn = "Akshit"
+
+// let boolLoggedIn = Boolean(isLoggedIn)
+// console.log(boolLoggedIn);
+// 1=>true
+// 0 => false
+// "" => false
+// "akshit" => true
+
+let someNum = 33
+let strNum  = String(someNum)
+console.log(typeof strNum);
