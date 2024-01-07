dcb0919fec12ee02bb7da02362fa54ee7062c300
Akshit <129672950+akscpp@users.noreply.github.com>
2024-01-07 12:23:57 +0000

    conversion and comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,6 +20,34 @@
 // "" => false
 // "akshit" => true
 
-let someNum = 33
-let strNum  = String(someNum)
-console.log(typeof strNum);
+// let someNum = 33
+// let strNum  = String(someNum)
+// console.log(typeof strNum);
+
+// #####################################Operations##########################################
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(3 + 4 * 5 % 3); 
+let str1 = "akshit"
+let str2 = " raj"
+
+// console.log(str1+str2);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+"");
+
+let num1, num2,num3
+num1 = num2 =  num3 = 2 + 2
+
+let gameCont = 100
+++gameCont
+console.log(gameCont);
+
+
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//Compare two same datatypes
+
+
+// avoid these comparisons #clean code
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+
+//The reason is that an equality check == and comprison > < >= <= work differently. Comparisons convert null to a number ,
+// treating it as 0;
+console.log(undefined == 0)
+console.log(undefined>0)
+console.log(undefined>=0);
+
+// strict check => === (check values and datatypes)
+
+console.log("2" == 2); //true as conversion happens
+console.log("2" === 2);//false as datatype is different (conversion does not happen)
