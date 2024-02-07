0a7d4f6a420ed689fd3583cce9a471ceaab1ad9b
Akshit <129672950+akscpp@users.noreply.github.com>
2024-02-07 18:46:52 +0000

    new

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,4 +5,32 @@ console.log(name + repoCount + " Value")//Not recommended
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)//recommended
 
-const gameName = new String('GTA VI')
+const gameName = new String('Akshit-AK')
+
+// console.log(typeof gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('A'));
+// console.log(gameName);
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+const anotherString = gameName.slice(-7,5);
+
+console.log(anotherString);
+
+const newSTringOne = "aks-akshit-raj";
+console.log(newSTringOne);
+console.log(newSTringOne.trim());
+
+const url = "https://akshit.com/akshit%20raj"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('akshit'));
+
+console.log(newSTringOne.split('-'))
